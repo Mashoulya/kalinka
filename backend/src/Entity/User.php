0d06f7221bb097558f16a883096cfc63832ba9a5
MariaIb <maria_ib@yahoo.com>
2026-03-30 17:34:51 +0200

    User entity update

--- backend/src/Entity/User.php
+++ backend/src/Entity/User.php
@@ -50,6 +50,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private ?\DateTimeImmutable $updatedAt = null;
 
+    #[ORM\Column]
+    private array $roles = [];
+
     public function __construct()
     {
         $this->createdAt = new \DateTimeImmutable();
@@ -164,7 +167,16 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     public function getRoles(): array
     {
-        return ['ROLE_USER'];
+        $roles = $this->roles;
+        $roles[] = 'ROLE_USER';
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): static
+    {
+        $this->roles = $roles;
+
+        return $this;
     }
 
     public function eraseCredentials(): void {}
