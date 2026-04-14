b03a58375c75773f3d86619739a97c7c16b2adde
MariaIb <maria_ib@yahoo.com>
2026-04-14 22:22:57 +0200

    feat: create ProfileController

--- backend/src/Controller/ProfileController.php
+++ backend/src/Controller/ProfileController.php
@@ -2,35 +2,50 @@
 
 namespace App\Controller;
 
+use App\Entity\Order;
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ProfileController extends AbstractController
 {
-    #[Route('/api/me', name: 'app_me', methods: ['GET'])]
+    #[Route('/api/me', name: 'api_me', methods: ['GET'])]
     public function index(): JsonResponse
     {
-        if(!$this->getUser()) {
-            return $this->json(['error' => 'Unauthorized'], 401);
+        $user = $this->getUser();
+        if (!$user instanceof User) {
+            return $this->json(['error' => 'User not authenticated'], JsonResponse::HTTP_UNAUTHORIZED);
         }
 
-        $user = $this->getUser();
+        $orders = $user->getOrders()->toArray();
+        usort(
+            $orders,
+            static fn (Order $a, Order $b): int => $b->getCreatedAt()->getTimestamp() <=> $a->getCreatedAt()->getTimestamp()
+        );
+
+        $ordersData = array_map(
+            static fn (Order $order): array => [
+                'number' => $order->getId(),
+                'date' => $order->getCreatedAt()?->format('Y-m-d H:i:s'),
+                'price' => $order->getTotal(),
+                'status' => $order->getStatus()?->value,
+            ], $orders);
 
         return $this->json([
-            'email' => $user->getEmail(),
-            'password' => $user->getPassword(),
-            'title' => $user->getTitle(),
-            'lastName' => $user->getLastName(),
-            'firstName' => $user->getFirstName(),
-            'birthday' => $user->getBirthday() ? $user->getBirthday()->format('Y-m-d') : null,
-            'postalCode' => $user->getPostalCode(),
-            'city' => $user->getCity(),
-            'phone' => $user->getPhone(),
-        ]);
-
-        return $this->json([
-            
+            'orders' => $ordersData,
+            'credentials' => [
+                'email' => $user->getEmail(),
+            ],
+            'profile' => [
+                'title' => $user->getTitle()?->value,
+                'lastName' => $user->getLastName(),
+                'firstName' => $user->getFirstName(),
+                'birthDate' => $user->getBirthDate()?->format('Y-m-d'),
+                'postalCode' => $user->getPostalCode(),
+                'city' => $user->getCity(),
+                'phone' => $user->getPhone(),
+            ],
         ]);
     }
 }
