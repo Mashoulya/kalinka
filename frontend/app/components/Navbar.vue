a1c84af5e50eb99813573ac02acd46f085f7c598
MariaIb <maria_ib@yahoo.com>
2026-04-16 20:22:59 +0200

    Setup frontend configuration

---
+++ frontend/app/components/Navbar.vue
@@ -0,0 +1,7 @@
+<template>
+  <nav class="bg-black text-white p-4 flex gap-4">
+    <NuxtLink to="/">Accueil</NuxtLink>
+    <NuxtLink to="/products">Produits</NuxtLink>
+    <NuxtLink to="/cart">Panier</NuxtLink>
+  </nav>
+</template>
---
+++ frontend/app/pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1 class="text-2xl font-bold">
+    Accueil e-commerce
+  </h1>
+</template>
