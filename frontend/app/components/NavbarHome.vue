71421d51221786fe5f9243872b96a95df093d2f6
MariaIb <maria_ib@yahoo.com>
2026-04-21 00:42:25 +0200

    Create home navbar component

--- frontend/app/components/NavbarHome.vue
+++ frontend/app/components/NavbarHome.vue
@@ -1,36 +1,32 @@
 <template>
-  <div class="w-full">
-
+  <div class="w-full bg-black-hover font-sora">
     <!-- TOP BAR -->
-    <div class="bg-black text-white text-sm py-2">
-      <div class="max-w-7xl mx-auto flex justify-between px-4">
-
-        <div class="flex gap-6">
-
-          <div class="flex items-center gap-2">
-            <span>📞</span>
-            <span>0205040102</span>
-          </div>
-
-          <div class="flex items-center gap-2">
-            <span>📍</span>
-            <span>Saint-Jacques-de-la-Lande</span>
-          </div>
-
+    <div
+      class="bg-black-topnav text-white text-sm py-2 border-b border-black-stroke"
+    >
+      <div class="max-w-7xl mx-auto flex justify-start px-4 gap-6">
+        <div class="flex items-center gap-2">
+          <img src="/logos/tel.png" alt="logo-phone" />
+          <span>0205040102</span>
         </div>
 
+        <div class="flex items-center gap-2">
+          <img src="/logos/gps_marker.png" alt="logo-location" />
+          <span>Saint-Jacques-de-la-Lande</span>
+        </div>
       </div>
     </div>
 
     <!-- BOTTOM BAR -->
-    <div class="bg-white border-b">
-      <div class="max-w-7xl mx-auto flex items-center justify-between px-4 py-4">
-
+    <div class="bg-black-nav border-b border-black-stroke">
+      <div
+        class="max-w-7xl mx-auto flex items-center justify-between px-4 py-4"
+      >
         <!-- LOGO -->
-        <img src="" class="h-10" />
+        <img src="/logos/logo_blanc.png" alt="logo" class="h-10" />
 
         <!-- NAV LINKS -->
-        <ul class="flex gap-8 font-medium text-gray-700">
+        <ul class="flex gap-8 font-medium text-white text-base">
           <li><NuxtLink to="/">Accueil</NuxtLink></li>
           <li><NuxtLink to="/shop">Boutique</NuxtLink></li>
           <li><NuxtLink to="/about">À propos</NuxtLink></li>
@@ -40,12 +36,14 @@
 
         <!-- ICONS -->
         <div class="flex gap-4 text-xl">
-          <NuxtLink to="/cart">🛒</NuxtLink>
-          <NuxtLink to="/profile">👤</NuxtLink>
+          <NuxtLink to="/cart">
+            <img src="/logos/icon_cart.png" alt="logo-cart" />
+          </NuxtLink>
+          <NuxtLink to="/profile">
+            <img src="/logos/icon_profile.png" alt="logo-profile" />
+          </NuxtLink>
         </div>
-
       </div>
     </div>
-
   </div>
 </template>
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,5 +2,33 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss'],
+  css: ['~/assets/css/fonts.css'],
+
+  tailwindcss: {
+    config: {
+      theme: {
+        extend: {
+          fontFamily: {
+            sora: ['Sora', 'sans-serif']
+          },
+          colors: {
+            red: {
+              light: '#C1121F',  // rouge primaire
+              dark: '#8b1a1a',  // rouge foncé
+            },
+            green: {
+              DEFAULT: '#3D6245',
+            },
+            black: {
+              hover: '#222222',
+              topnav: '#17171799',
+              nav: '#171717',
+              stroke: '#39393A'
+            },
+          }
+        }
+      }
+    }
+  }
 })
