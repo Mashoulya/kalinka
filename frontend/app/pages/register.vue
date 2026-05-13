79f321b00381a7c8d03fb15b20e5fa87cfa84c9a
MariaIb <maria_ib@yahoo.com>
2026-05-13 11:41:48 +0200

    Fix: frontend login page

---
+++ frontend/app/pages/register.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <section class="py-10 px-5 md:px-20 md:py-20">
+        <h1 class="text-red-light text-3xl font-bold text-center mb-14 md:text-5xl">Création de compte</h1>
+    </section>
+</template>
