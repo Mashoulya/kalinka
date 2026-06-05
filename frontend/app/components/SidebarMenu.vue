b192f40eea09ee13e7994a99d150c5a375a9cd58
MariaIb <maria_ib@yahoo.com>
2026-06-05 16:38:40 +0200

    WIP: sidebar menu styling

--- frontend/app/components/SidebarMenu.vue
+++ frontend/app/components/SidebarMenu.vue
@@ -2,5 +2,34 @@
 </script>
 
 <template>
-    
+    <aside>
+        <nav>
+            <ul class="font-semibold">
+                <li class="flex items-center">
+                    <img src="/logos/icon-label.svg" class="h-10 w-10" alt="">
+                    <NuxtLink to="#">Promotion</NuxtLink>
+                </li>
+                <li class="flex items-center">
+                    <img src="/logos/icon-heart.svg" class="h-4 w-4" alt="">
+                    <NuxtLink to="#">Les plus aimés</NuxtLink> 
+                </li>
+                <li class="flex items-center">
+                    <img src="/logos/icon-category.svg" class="h-4 w-4" alt="">
+                    <span>Catégories</span> 
+                </li>
+                <li>
+                    <div class="collapse collapse-arrow">
+                        <input type="checkbox" />
+                        <div class="collapse-title">Catégorie 1</div>
+                        <div class="collapse-content">
+                            <ul>
+                                <li><NuxtLink to="#">Soucatégorie 1</NuxtLink></li>
+                                <li><NuxtLink to="#">Soucatégorie 2</NuxtLink></li>
+                            </ul>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </nav>
+    </aside>
 </template>
--- frontend/app/pages/shop.vue
+++ frontend/app/pages/shop.vue
@@ -4,6 +4,13 @@
 </script>
 
 <template>
-  <h1>Page de la boutique</h1>
+  <section class="grid grid-cols-1 md:grid-cols-[auto_minmax(0,1fr)_220px] bg-white-section">
+    <SidebarMenu />
+    <h1 class="text-red-light text-3xl font-bold text-center mb-14 md:text-5xl">Boutique</h1>
+    <aside>
+      Panier
+    </aside>
+  </section>
+  
   
 </template>
